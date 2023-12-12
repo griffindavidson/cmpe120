90209f8cf13483f4a20ce7bc0c3ba7f0f7b342ba
lac-phong <quanglac03@gmail.com>
2023-12-12 00:36:23 -0800

    got rid of filler code

--- src/sim/Main.java
+++ src/sim/Main.java
@@ -137,11 +137,23 @@ public class Main
     public static void main(String[] args) 
     {
     	Scanner consoleReader = new Scanner(System.in);
+    	boolean breakpoint = false;
+        String[] breakpoints = new String[5];
+        int breakpointCount = 0;
 		debug = false;
 
 		System.out.println("Welcome to a RISC-V simulator \n" + "Made by Griffin, Lac, Viet, Karan, and Ali");
 		while (true) {
 			System.out.print("1: Run a program\n" + "2: Debugging\n" + "3: Exit \n");
+//			System.out.println("Select an option:");
+//            System.out.println("1 - Run a program");
+//            System.out.println("2 - Run the next instruction and then stop and wait for the next command.");
+//            System.out.println("3 - Return the contents of the register from the register file (x0 must always stay 0).");
+//            System.out.println("4 0x12345678 - Return the contents from the address 0x12345678 in the data memory.");
+//            System.out.println("5 - Return the value of the PC.");
+//            System.out.println("6 insn - Print the assembly of the instruction that will be executed next.");
+//            System.out.println("7 [pc] - Put a breakpoint at a particular/specified [pc].");
+//            System.out.println("8 - Continue execution until it hits the next breakpoint pc or exits.");
 			int number = getScannerInt(consoleReader);
 			if (number == 1)
 				runProgram(consoleReader);
@@ -152,65 +164,6 @@ public class Main
 		}
 		System.out.println("Thank you for using the simulator");
 		
-
-		// boolean exitProgram = false;
-        // boolean breakpoint = false;
-        // String[] breakpoints = new String[5];
-        // int breakpointCount = 0;
 		
-		// // currently an infinite loop
-        // while (!exitProgram) {
-        //     System.out.println("Select an option:");
-        //     System.out.println("r - Run the entire program in one go till it hits a breakpoint or exits.");
-        //     System.out.println("s - Run the next instruction and then stop and wait for the next command.");
-        //     System.out.println("rf - Return the contents of the register from the register file (x0 must always stay 0).");
-        //     System.out.println("0x12345678 - Return the contents from the address 0x12345678 in the data memory.");
-        //     System.out.println("pc - Return the value of the PC.");
-        //     System.out.println("insn - Print the assembly of the instruction that will be executed next.");
-        //     System.out.println("b [pc] - Put a breakpoint at a particular/specified [pc].");
-        //     System.out.println("c - Continue execution until it hits the next breakpoint pc or exits.");
-
-        //     String option = scan.nextLine();
-
-        //     switch (option) {
-        //         case "r":
-        //             // Implement logic for running the entire program till a breakpoint is hit or exits
-        //             break;
-        //         case "s":
-        //             // Implement logic for running the next instruction and then stopping
-        //             break;
-        //         case "rf":
-        //             System.out.println("Select a register to obtain its cocntents\nRegisters x0 to x31 (integer value)");
-		// 			int register = scan.nextInt();
-		// 			scan.nextLine();
-		// 			System.out.println("x" + register + ": " + cpu.getReg()[register]);
-        //         case "0x12345678":
-        //             // Implement logic for returning contents from a specified address in data memory
-        //             break;
-        //         case "pc":
-        //             // Implement logic for returning the value of the PC
-        //             break;
-        //         case "insn":
-        //             // Implement logic for printing the assembly of the next instruction
-        //             break;
-        //         case "b [pc]":
-        //             // Implement logic for putting a breakpoint at a specified [pc]
-        //             break;
-        //         case "c":
-                    
-        //             break;
-        //         default:
-        //             System.out.println("Invalid option. Please try again.");
-        //     }
-		// }
-		// test.readFile("addi_nohazard.dat");
-		// test.readFile("addi_hazards.dat");
-		// test.readFile("i_type.dat");
-    	// test.readFile("r_type.dat");
-
-    	// test.readAddiNoHazard();
-    	// test.readAddiHazards();
-    	// test.readIType();
-    	// test.readRType();
     }
 }
