0d32a8834757c7a9c0e41f2ea3c3d21fa54c2122
Yuuji1140 <vietnguyen2358@gmail.com>
2023-12-11 16:27:06 -0800

    Update Reader & Main

--- src/sim/Main.java
+++ src/sim/Main.java
@@ -1,14 +1,79 @@
 package sim;
 
+import java.util.Scanner;
+
 public class Main 
 {
     public static void main(String[] args) 
     {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("File to read: ");
+		String fileName = scan.nextLine();
+
     	Reader test = new Reader();
-    	
-    	test.readAddiNoHazard();
-    	test.readAddiHazards();
-    	test.readIType();
-    	test.readRType();
+		test.readFile(fileName);
+
+		boolean exitProgram = false;
+        boolean breakpoint = false;
+        String[] breakpoints = new String[5];
+        int breakpointCount = 0;
+
+		test.getInstructions();
+		CPU cpu = new CPU("");
+		
+		// currently an infinite loop
+        while (!exitProgram) {
+            System.out.println("Select an option:");
+            System.out.println("r - Run the entire program in one go till it hits a breakpoint or exits.");
+            System.out.println("s - Run the next instruction and then stop and wait for the next command.");
+            System.out.println("rf - Return the contents of the register from the register file (x0 must always stay 0).");
+            System.out.println("0x12345678 - Return the contents from the address 0x12345678 in the data memory.");
+            System.out.println("pc - Return the value of the PC.");
+            System.out.println("insn - Print the assembly of the instruction that will be executed next.");
+            System.out.println("b [pc] - Put a breakpoint at a particular/specified [pc].");
+            System.out.println("c - Continue execution until it hits the next breakpoint pc or exits.");
+
+            String option = scan.nextLine();
+
+            switch (option) {
+                case "r":
+                    // Implement logic for running the entire program till a breakpoint is hit or exits
+                    break;
+                case "s":
+                    // Implement logic for running the next instruction and then stopping
+                    break;
+                case "rf":
+                    System.out.println("Select a register to obtain its cocntents\nRegisters x0 to x31 (integer value)");
+					int register = scan.nextInt();
+					scan.nextLine();
+					System.out.println("x" + register + ": " + cpu.getReg()[register]);
+                case "0x12345678":
+                    // Implement logic for returning contents from a specified address in data memory
+                    break;
+                case "pc":
+                    // Implement logic for returning the value of the PC
+                    break;
+                case "insn":
+                    // Implement logic for printing the assembly of the next instruction
+                    break;
+                case "b [pc]":
+                    // Implement logic for putting a breakpoint at a specified [pc]
+                    break;
+                case "c":
+                    
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+		}
+		// test.readFile("addi_nohazard.dat");
+		// test.readFile("addi_hazards.dat");
+		// test.readFile("i_type.dat");
+    	// test.readFile("r_type.dat");
+
+    	// test.readAddiNoHazard();
+    	// test.readAddiHazards();
+    	// test.readIType();
+    	// test.readRType();
     }
 }
--- src/sim/Reader.java
+++ src/sim/Reader.java
@@ -7,12 +7,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Reader {
+	private ArrayList<String> instructions = new ArrayList<>();
 
-	private void readFile(String fileName) {
+	public void readFile(String fileName) {
 		FileReader reader = null;
 		BufferedReader buffer = null;
 		String instruction = null;
-		
+
 		try {
 			reader = new FileReader(fileName);
 		} catch (FileNotFoundException e) {
@@ -20,44 +21,46 @@ public class Reader {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
 		buffer = new BufferedReader(reader);
-		
+
+		System.out.println("Reading: " + fileName);
 		try {
 			buffer.readLine(); // skip spacer byte
 			while (true)
 			{
-				instruction = buffer.readLine();
+				String readByte = buffer.readLine();
+				instruction = readByte;
+
+				if(instruction == null) break;
+				instructions.add(readByte);
 				// Every file has x/4 lines, since each line is one byte and each file needs to have
 				// 4 bytes per instruction
 				for (int i = 0; i < 3; i++)
 				{
-					String readByte = buffer.readLine();
-					
-					instruction += readByte;
-					
-					if (readByte == null)
-					{
+					readByte = buffer.readLine();
+
+					if (readByte == null) {
 						instruction = null;
 						break;
 					}
+					instruction += readByte;
+					instructions.add(readByte);
 				}
-				
-				if (instruction == null) {
-					System.out.println(""); // spacer for output
-					break;
-				}
-				
+
 				// perhaps some instruction execution block should go here
 				// since by now a complete instruction has formed.... we may
 				// not need the arraylist to hold instructions
+
+				// instructions.add(instruction);
 				
-				String hex = convertToHex(instruction);		
-				
-				System.out.println(hex);
-				
-				String opcode = instruction.substring(25);
-				System.out.println("Opcode: " + opcode);
+
+				if (instruction != null && instruction.length() > 25) {
+					System.out.println(instruction);
+					String opcode = instruction.substring(25);
+					System.out.println("Opcode: " + opcode);
+				}
+
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -67,84 +70,46 @@ public class Reader {
 			e.printStackTrace();
 		}
 	}
-	
-	public void readAddiNoHazard() {
-		System.out.println("Reading: addi_nohazard.dat");
-		readFile("addi_nohazard.dat");
-	}
-	
-	public void readAddiHazards() {
-		System.out.println("Reading: addi_hazards.dat");
-		readFile("addi_hazards.dat");
-	}
-	
-	public void readIType() {
-		System.out.println("Reading: i_type.dat");
-		readFile("i_type.dat");
-	}
-	
-	public void readRType() {
-		System.out.println("Reading: r_type.dat");
-		readFile("r_type.dat");
-	}
-	
-	private String convertToHex(String instruction)
-	{
-		String hex = "";
-		
-		for (int i = 0; i < instruction.length(); i += 4) {
-			String value = instruction.substring(i, i + 4);
-			
-			if (value.equals("0000")) {
-				hex += "0";
-			}
-			else if (value.equals("0001")) {
-				hex += "1";
-			}
-			else if (value.equals("0010")) {
-				hex += "2";
-			}
-			else if (value.equals("0011")) {
-				hex += "3";
-			}
-			else if (value.equals("0100")) {
-				hex += "4";
-			}
-			else if (value.equals("0101")) {
-				hex += "5";
-			}
-			else if (value.equals("0110")) {
-				hex += "6";
-			}
-			else if (value.equals("0111")) {
-				hex += "7";
-			}
-			else if (value.equals("1000")) {
-				hex += "8";
-			}
-			else if (value.equals("1001")) {
-				hex += "9";
-			}
-			else if (value.equals("1010")) {
-				hex += "a";
-			}
-			else if (value.equals("1011")) {
-				hex += "b";
-			}
-			else if (value.equals("1100")) {
-				hex += "c";
-			}
-			else if (value.equals("1101")) {
-				hex += "d";
-			}
-			else if (value.equals("1110")) {
-				hex += "e";
-			}
-			else {
-				hex += "f";
-			}
-		}
-		
-		return hex;
-	}
+
+	public ArrayList<String> getInstructions() {
+        return instructions;
+    }
+
+	public ArrayList<String> getInstructionStrings() {
+        ArrayList<String> instructionStrings = new ArrayList<>();
+        StringBuilder instructionBuilder = new StringBuilder();
+
+        for (int i = 0; i < instructions.size(); i += 4) {
+            for (int j = 0; j < 4; j++) {
+                if (i + j < instructions.size()) {
+                    instructionBuilder.append(instructions.get(i + j));
+                }
+            }
+
+            instructionStrings.add(instructionBuilder.toString());
+            instructionBuilder.setLength(0); // Clear the StringBuilder for the next iteration
+        }
+
+        return instructionStrings;
+    }
+
+	// public void readAddiNoHazard() {
+	// 	System.out.println("Reading: addi_nohazard.dat");
+	// 	readFile("addi_nohazard.dat");
+	// }
+
+	// public void readAddiHazards() {
+	// 	System.out.println("Reading: addi_hazards.dat");
+	// 	readFile("addi_hazards.dat");
+	// }
+
+	// public void readIType() {
+	// 	System.out.println("Reading: i_type.dat");
+	// 	readFile("i_type.dat");
+	// }
+
+	// public void readRType() {
+	// 	System.out.println("Reading: r_type.dat");
+	// 	readFile("r_type.dat");
+	// }
 }
